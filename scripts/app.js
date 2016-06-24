dff5a1c4809de0dc9a7425d544159fdf30c02ca9
avj2352 <avj2352@gmail.com>
2016-06-24 20:21:18 +0530

    Integrated BigVideo Plugin

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,24 @@
+// jQuery IIFE
+(function($){
+  var $video  = $('video'),
+    $window = $(window);
+
+$(window).resize(function(){
+
+    var height = $window.height();
+    $video.css('height', height);
+    var videoWidth = $video.width(),
+        windowWidth = $window.width(),
+    marginLeftAdjust =   (windowWidth - videoWidth) / 2;
+
+    $video.css({
+        'height': height,
+        'marginLeft' : '0px',
+        'marginTop' : '-25px'
+    });
+}).resize();
+}(jQuery));//IIFE jQuery
+
 (function(){
   var myApp = angular.module('myApp',['ngRoute','ngAnimate']);
 }());//IIFE
